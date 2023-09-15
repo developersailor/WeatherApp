87bb40ca11a694a12ad37b854a0f4d888ed0369a
Mehmet Fıșkındal <developersailor@gmail.com>
2023-09-16 00:46:54 +0300

    mapkit ile map eklendi

--- WeatherApp/Features/MyWeather/MyWeatherView.swift
+++ WeatherApp/Features/MyWeather/MyWeatherView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 import CoreLocation
 import Combine
-
+import MapKit
 struct LocationData: Codable {
     let cityName: String
 }
@@ -11,12 +11,10 @@ struct MyWeatherView: View {
     @StateObject private var locationManager = LocationManager()
     @ObservedObject private var myWeatherViewModel: MyWeatherViewModel = MyWeatherViewModel()
     
+
     var body: some View {
         VStack {
-            
-                //Button ile arama isteği atılacak
-                // CoreLocation kütüphanesinden aldığım koordinatlar ile OpenWeatherMap API'sine istek atarak hava durumu bilgilerini alıyorum.
-                
+     
                 Button("Get Local Weather") {
                     locationManager.requestLocation()
                     if let location = locationManager.location {
@@ -26,6 +24,7 @@ struct MyWeatherView: View {
                     }
                 }.padding()
             if let degree = myWeatherViewModel.degree, let city = myWeatherViewModel.cityName, let humidity = myWeatherViewModel.humidity {
+             
                 Text("\(degree.formatted())°C")
                     .font(.system(size: 50))
                     .bold()
@@ -33,6 +32,11 @@ struct MyWeatherView: View {
                     .font(.system(size: 30))
                 Text("Humidity: \(humidity)")
                     .font(.system(size: 20))
+                
+            //locationmanagerden gelen verilere göre MapKitte şehir görüntülenecek.
+                Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0, longitude: locationManager.location?.coordinate.longitude ?? 0), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))), interactionModes: .all, showsUserLocation: true, userTrackingMode: .constant(.follow))
+                    .frame(height: 300)
+                    .padding()
             }
         }
     }
