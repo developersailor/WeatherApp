5fe0517ddd590f08b132a381a0f91b8b67252425
mehmet fiskindal <mehmetfiskindal@gmail.com>
2024-06-20 16:14:13 +0300

    chart 🥳 added

--- WeatherApp/Features/Forecast/ForecastView.swift
+++ WeatherApp/Features/Forecast/ForecastView.swift
@@ -6,11 +6,12 @@
 //
 
 import SwiftUI
+import Charts
+
 
 struct ForecastView: View {
   @ObservedObject private var forecastViewModel: ForecastViewModel = ForecastViewModel()
   @State private var city: String = ""
-  
   
   var body: some View {
     NavigationView {
@@ -33,20 +34,24 @@ struct ForecastView: View {
         }
         .padding(.all, 10.0)
         
+        WeatherChartView(forecastViewModel: forecastViewModel,
+                         kelvinToCelsius: forecastViewModel.kelvinToCelsius,
+                         dateFormatter: forecastViewModel.dateFormatter,
+                         forecastList: forecastViewModel.forecastList ?? [])
+          .frame(height: 300)
+        
         ForecastListView(forecastViewModel: forecastViewModel,
                          kelvinToCelsius: forecastViewModel.kelvinToCelsius,
                          dateFormatter: forecastViewModel.dateFormatter,
                          forecastList: forecastViewModel.forecastList ?? [])
       }
+      .navigationBarTitle("Hava Durumu")
+      .navigationBarItems(trailing: Button(action: {}) {
+        Image(systemName: "gearshape")
+      })
     }
-    .navigationBarTitle("Hava Durumu")
-    .navigationBarItems(trailing: Button(action: {}) {
-      Image(systemName: "gearshape")
-    })
   }
 }
-
-
 struct ForecastListView: View {
   var forecastViewModel: ForecastViewModel
   var kelvinToCelsius: (Double) -> Int
@@ -54,25 +59,30 @@ struct ForecastListView: View {
   var forecastList: [Liste]
 
   var body: some View {
-    List {
-      ForEach(forecastList.indices, id: \.self) { index in
-        let data = forecastList[index]
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack(alignment: .top, spacing: 10) {
+        ForEach(forecastList.indices, id: \.self) { index in
+          let data = forecastList[index]
 
-        if let date = dateFormatter.date(from: data.dtTxt ?? "") {
-          Section(
-            header: Text(date, style: .date)
-              .font(.headline)
-              .foregroundColor(.blue)
-              .padding(.vertical, 5)
-          ) {
-            ForecastListViewItem(data: data, kelvinToCelsius: kelvinToCelsius)
+          if let date = dateFormatter.date(from: data.dtTxt ?? "") {
+            VStack(alignment: .leading) {
+              Text(date, style: .date)
+                .font(.headline)
+                .foregroundColor(.blue)
+                .padding(.vertical, 5)
+
+              ForecastListViewItem(data: data, kelvinToCelsius: kelvinToCelsius)
+            }
+            .frame(width: 200)  // Adjust the width as needed
+            .background(Color(UIColor.systemGroupedBackground))
+            .cornerRadius(10)
+            .shadow(radius: 2)
           }
-          .listRowBackground(Color(UIColor.systemGroupedBackground))
         }
       }
+      .padding(.horizontal, 10)
     }
-    .listStyle(.insetGrouped)
-    .accentColor(.blue)
+    .padding(.vertical, 10)
   }
 }
 
@@ -80,7 +90,7 @@ struct ForecastListViewItem: View {
   let data: Liste
   let kelvinToCelsius: (Double) -> Int
   var body: some View {
-    VStack(alignment: .leading) {
+    VStack(alignment: .center) {
       VStack(alignment: .leading, spacing: 5) {
         HStack{
           Text("Sıcaklık:")
@@ -109,7 +119,43 @@ struct ForecastListViewItem: View {
     .padding(.vertical, 10)
   }
 }
-
+struct WeatherChartView: View {
+  var forecastViewModel: ForecastViewModel
+  var kelvinToCelsius: (Double) -> Int
+  var dateFormatter: DateFormatter
+  var forecastList: [Liste]
+  
+  var body: some View {
+    Chart {
+      ForEach(forecastList, id: \.dt) { data in
+        if let dateStr = data.dtTxt,
+           let date = dateFormatter.date(from: dateStr) {
+          LineMark(
+            x: .value("Date", date),
+            y: .value("Temperature", kelvinToCelsius(data.main?.temp ?? 0))
+          )
+          .foregroundStyle(Color.blue)
+          .symbol(Circle())
+        }
+      }
+    }
+    .chartXAxis {
+      AxisMarks(values: .stride(by: .hour, count: 12)) {
+        AxisGridLine()
+        AxisTick()
+        AxisValueLabel(format: .dateTime.hour().month().day().hour(), centered: true, orientation: .vertical)
+      }
+    }
+    .chartYAxis {
+      AxisMarks() {
+        AxisGridLine()
+        AxisTick()
+        AxisValueLabel()
+      }
+    }
+    .padding()
+  }
+}
 #Preview {
   ForecastView()
 }
